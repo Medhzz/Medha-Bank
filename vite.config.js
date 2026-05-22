3ec9cc32de3d28d510dfdf9d78cc6ebb2dfce73d
Medhzz <medha.psbb@gmail.com>
2026-05-22 09:48:38 +0530

    add deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/Medha-Bank/",
   plugins: [react()],
-  server: { port: 5173, host: true },
+  server: {
+    port: parseInt(process.env.PORT || "5173"),
+    host: true,
+  },
 });
